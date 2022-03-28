6ba93cd3c08ee9a3ef0de8ef184fedc49d831d89
Shreya Chaturvedi <90124537+Pixeliate@users.noreply.github.com>
2022-03-28 22:44:20 +0530

    Add files via upload

--- speed_perturbation.sh
+++ speed_perturbation.sh
@@ -8,22 +8,22 @@ path="/media/speechlab/A2/Shreya/TLT_2021/ETLT2021_CAMBRIDGE_EN_baseline/ETLT202
 format="wav"
 #target will be the folder in which audio will be prefix of directory created. Mention which audio it is in either of case[corpus , train . test , dev ] 
 target="train_GAN_"
-#Add what factor of tempo you need to provide. Keeping 1 as original audio 
+#Add what factor of speed you need to provide. Keeping 1 as original audio 
 factor="1.2"
-#Tempo based suffix for folde and Prefix for regenerated audio. 
-tempo="tempo_1_2"
+#speed based suffix for folde and Prefix for regenerated audio. 
+speed="speed_1_2"
 
-mkdir $target$tempo
+mkdir $target$speed
 
 x=$(find $path -name "*.$format")
 #echo "After Replacement:" ${filename//not/aa}
 for i in $x; do # Not recommended, will break on whitespace
     #echo $i
-    IFS='/' 
+    IFS='/'
     read -a o1 <<<"$i" #reading str as an array as tokens separated by IFS
     #echo ${o1[-1]}
-    out="$target$tempo/$tempo$o1"
-    #ffmpeg -i $i -filter:a "atempo=$factor" $out
-    echo $out
+    out="$target$speed/$speed$o1"
+    sox $i $out speed $factor
+    #echo $out
     #break
 done
